1a3bc8859c0e44a4ad205f428ddc9eff9a18cc1f
itsapumpkinpie <itsapumpkinpie@gmail.com>
2022-08-17 17:07:23 +0300

    test commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='cbt-dictionary',
+    version='0.0.1',
+    author="itsapumpkinpie",
+    author_email="itsapumpkinpie@gmail.com",
+    description='Fast api app',
+    scripts=['app/main.py']
+)
