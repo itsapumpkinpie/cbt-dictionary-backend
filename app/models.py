2746cf67e1ee2477f895e93a92aedd6a180f07a0
Mashkova Oksana <91336056+itsapumpkinpie@users.noreply.github.com>
2022-10-25 18:07:31 +0300

    Delete models.py

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-
-def connect_db():
-    engine = create_engine('', connect_args={})
-    session = Session(bind=engine.connect())
-    return session
