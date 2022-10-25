bb209aaf3e4a1461617c01a7a47eda677a43b702
itsapumpkinpie <itsapumpkinpie@gmail.com>
2022-10-25 18:18:31 +0300

    Start again

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup
 
-
 setup(
-    name='cbt-dictionary',
-    version='0.0.1',
-    author="itsapumpkinpie",
-    author_email="itsapumpkinpie@gmail.com",
-    description='Fast api app',
-    scripts=['app/main.py']
+    scripts=["app/main.py"]
 )
