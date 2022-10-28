bb6117b09ec27245e6bc19415797362edbff624a
itsapumpkinpie <itsapumpkinpie@gmail.com>
2022-10-28 13:02:28 +0300

    add new routers

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 import uvicorn
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/posts")
+def read_posts():
+    return {"massage": "our posts"}
+
+@app.get("/posts/{post_id}")
+def get_one_post(post_id: int):
+    return {"post_id": post_id}
+
+
+@app.post("/createpost")
+def create_post(postData: dict = Body(...)):
+    print(postData)
+    return {"new_post": f"title: {postData['title']} content:  {postData['content']}"}
+
+
+@app.put("/update_post")
+def update_post():
+    return {"massage": "post the post data has been updated"}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
