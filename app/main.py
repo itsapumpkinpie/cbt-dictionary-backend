3a80ebf96d6dfe821e5ae161c13534e5ab36a2b7
itsapumpkinpie <itsapumpkinpie@gmail.com>
2022-11-03 21:35:53 +0300

    database connection test

--- app/main.py
+++ app/main.py
@@ -1,21 +1,34 @@
-from fastapi import FastAPI, status, HTTPException, Response
+from fastapi import FastAPI, status, HTTPException, Response, Depends
 import uvicorn
 from pydantic import BaseModel
 from random import randrange
+from sqlalchemy.orm import Session
+from .db import models
+from .db.сonnection import engine, SessionLocal
 
 
 class Post(BaseModel):
     post_id: int | None = None
-    title: str | None = None
-    emotions: str | None = None
-    thoughts: str | None = None
+    accident_title: str | None = None
+    description_emotions: str | None = None
+    description_thoughts: str | None = None
     body_reaction: str | None = None
     rational_thoughts: str | None
     rational_actions: str | None
     published: bool = True
 
 
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 my_future_database_posts = [
@@ -43,6 +56,12 @@ def find_index_post(post_id):
             return index
 
 
+# database connection test
+@app.get("/sqlalchemy")
+def read_users(db: Session = Depends(get_db)):
+    return {"successful return data"}
+
+
 @app.get("/posts")
 def get_posts():
     return {"data of all posts": my_future_database_posts}
@@ -50,7 +69,6 @@ def get_posts():
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-
     post_dict = post.dict()
     post_dict["post_id"] = randrange(0, 10000)
 
@@ -60,7 +78,6 @@ def create_posts(post: Post):
 
 @app.get("/posts/{post_id}")
 def get_post(post_id: int):
-
     post = find_one_post(post_id)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -71,7 +88,6 @@ def get_post(post_id: int):
 
 @app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(post_id: int):
-
     index = find_index_post(post_id)
     if not index:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -83,10 +99,9 @@ def delete_post(post_id: int):
 
 @app.put("/posts/{post_id}")
 def update_post(post_id: int, post: Post):
-
     index = find_index_post(post_id)
 
-    if index == None:
+    if index is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'post with id: {post_id} does not exist')
 
